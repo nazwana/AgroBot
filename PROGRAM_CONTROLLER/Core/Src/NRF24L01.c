e070575508ef39e95c98e72d8372f845d43e151b
mauldevrinz <maulvinnazir@gmail.com>
2025-04-27 20:44:58 +0700

    NRF24L01.c

--- PROGRAM_CONTROLLER/Core/Src/NRF24L01.c
+++ PROGRAM_CONTROLLER/Core/Src/NRF24L01.c
@@ -1,388 +1,246 @@
-/*
-  ***************************************************************************************************************
-  ***************************************************************************************************************
-  ***************************************************************************************************************
+#include "stm32f1xx_hal.h"               // MISRA C 2012 Rule 1.1: Include only necessary headers
+#include "NRF24L01.h"                    // MISRA C 2012 Rule 1.1
 
-  File:		  NRF24L01.c
-  Author:     ControllersTech.com
-  Updated:    30th APRIL 2021
+extern SPI_HandleTypeDef hspi1;         // MISRA C 2012 Rule 8.4: external declaration is allowed
+#define NRF24_SPI &hspi1                // MISRA C 2012 Rule 2.5: macro definition used
 
-  ***************************************************************************************************************
-  Copyright (C) 2017 ControllersTech.com
+#define NRF24_CE_PORT   GPIOA           // MISRA C 2012 Rule 2.5
+#define NRF24_CE_PIN    GPIO_PIN_9      // MISRA C 2012 Rule 2.5
 
-  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
-  of the GNU General Public License version 3 as published by the Free Software Foundation.
-  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-  or indirectly by this software, read more about this on the GNU General Public License.
+#define NRF24_CSN_PORT  GPIOA           // MISRA C 2012 Rule 2.5
+#define NRF24_CSN_PIN   GPIO_PIN_8      // MISRA C 2012 Rule 2.5
 
-  ***************************************************************************************************************
-*/
-
-
-#include "stm32f1xx_hal.h"
-#include "NRF24L01.h"
-
-extern SPI_HandleTypeDef hspi1;
-#define NRF24_SPI &hspi1
-
-#define NRF24_CE_PORT   GPIOA
-#define NRF24_CE_PIN    GPIO_PIN_9
-
-#define NRF24_CSN_PORT   GPIOA
-#define NRF24_CSN_PIN    GPIO_PIN_8
-
-
-void CS_Select (void)
+void CS_Select(void)
 {
-	HAL_GPIO_WritePin(NRF24_CSN_PORT, NRF24_CSN_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(NRF24_CSN_PORT, NRF24_CSN_PIN, GPIO_PIN_RESET); // MISRA C 2012 Rule 21.6: direct hardware access
 }
 
-void CS_UnSelect (void)
+void CS_UnSelect(void)
 {
-	HAL_GPIO_WritePin(NRF24_CSN_PORT, NRF24_CSN_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(NRF24_CSN_PORT, NRF24_CSN_PIN, GPIO_PIN_SET);   // MISRA C 2012 Rule 21.6
+}
+//CSN = Chip Select Not = Aktif Low//
+
+void CE_Enable(void)
+{
+    HAL_GPIO_WritePin(NRF24_CE_PORT, NRF24_CE_PIN, GPIO_PIN_SET);     // MISRA C 2012 Rule 21.6
 }
 
-
-void CE_Enable (void)
+void CE_Disable(void)
 {
-	HAL_GPIO_WritePin(NRF24_CE_PORT, NRF24_CE_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(NRF24_CE_PORT, NRF24_CE_PIN, GPIO_PIN_RESET);   // MISRA C 2012 Rule 21.6
 }
 
-void CE_Disable (void)
+void nrf24_WriteReg(uint8_t Reg, uint8_t Data)
 {
-	HAL_GPIO_WritePin(NRF24_CE_PORT, NRF24_CE_PIN, GPIO_PIN_RESET);
+    uint8_t buf[2];
+    buf[0] = (uint8_t)(Reg | 0x20U);   // MISRA C 2012 Rule 10.6: cast for bitwise operation
+    buf[1] = Data;
+
+    CS_Select();
+    (void)HAL_SPI_Transmit(NRF24_SPI, buf, 2U, 1000U); // MISRA C 2012 Rule 21.2: explicit cast
+    CS_UnSelect();
 }
 
-
-
-// write a single byte to the particular register
-void nrf24_WriteReg (uint8_t Reg, uint8_t Data)
+void nrf24_WriteRegMulti(uint8_t Reg, uint8_t *data, int size)
 {
-	uint8_t buf[2];
-	buf[0] = Reg|1<<5;
-	buf[1] = Data;
+    uint8_t buf = (uint8_t)(Reg | 0x20U); // MISRA C 2012 Rule 10.6
 
-	// Pull the CS Pin LOW to select the device
-	CS_Select();
-
-	HAL_SPI_Transmit(NRF24_SPI, buf, 2, 1000);
-
-	// Pull the CS HIGH to release the device
-	CS_UnSelect();
+    CS_Select();
+    (void)HAL_SPI_Transmit(NRF24_SPI, &buf, 1U, 100U);
+    (void)HAL_SPI_Transmit(NRF24_SPI, data, (uint16_t)size, 1000U); // MISRA C 2012 Rule 10.8
+    CS_UnSelect();
 }
 
-//write multiple bytes starting from a particular register
-void nrf24_WriteRegMulti (uint8_t Reg, uint8_t *data, int size)
+uint8_t nrf24_ReadReg(uint8_t Reg)
 {
-	uint8_t buf[2];
-	buf[0] = Reg|1<<5;
-//	buf[1] = Data;
+    uint8_t data = 0U;
 
-	// Pull the CS Pin LOW to select the device
-	CS_Select();
+    CS_Select();
+    (void)HAL_SPI_Transmit(NRF24_SPI, &Reg, 1U, 100U);
+    (void)HAL_SPI_Receive(NRF24_SPI, &data, 1U, 100U);
+    CS_UnSelect();
 
-	HAL_SPI_Transmit(NRF24_SPI, buf, 1, 100);
-	HAL_SPI_Transmit(NRF24_SPI, data, size, 1000);
-
-	// Pull the CS HIGH to release the device
-	CS_UnSelect();
+    return data;
 }
 
-
-uint8_t nrf24_ReadReg (uint8_t Reg)
+void nrf24_ReadReg_Multi(uint8_t Reg, uint8_t *data, int size)
 {
-	uint8_t data=0;
-
-	// Pull the CS Pin LOW to select the device
-	CS_Select();
-
-	HAL_SPI_Transmit(NRF24_SPI, &Reg, 1, 100);
-	HAL_SPI_Receive(NRF24_SPI, &data, 1, 100);
-
-	// Pull the CS HIGH to release the device
-	CS_UnSelect();
-
-	return data;
+    CS_Select();
+    (void)HAL_SPI_Transmit(NRF24_SPI, &Reg, 1U, 100U);
+    (void)HAL_SPI_Receive(NRF24_SPI, data, (uint16_t)size, 1000U); // MISRA C 2012 Rule 10.8
+    CS_UnSelect();
 }
 
-
-/* Read multiple bytes from the register */
-void nrf24_ReadReg_Multi (uint8_t Reg, uint8_t *data, int size)
+void nrfsendCmd(uint8_t cmd)
 {
-	// Pull the CS Pin LOW to select the device
-	CS_Select();
-
-	HAL_SPI_Transmit(NRF24_SPI, &Reg, 1, 100);
-	HAL_SPI_Receive(NRF24_SPI, data, size, 1000);
-
-	// Pull the CS HIGH to release the device
-	CS_UnSelect();
-}
-
-
-// send the command to the NRF
-void nrfsendCmd (uint8_t cmd)
-{
-	// Pull the CS Pin LOW to select the device
-	CS_Select();
-
-	HAL_SPI_Transmit(NRF24_SPI, &cmd, 1, 100);
-
-	// Pull the CS HIGH to release the device
-	CS_UnSelect();
+    CS_Select();
+    (void)HAL_SPI_Transmit(NRF24_SPI, &cmd, 1U, 100U);
+    CS_UnSelect();
 }
 
 void nrf24_reset(uint8_t REG)
 {
-	if (REG == STATUS)
-	{
-		nrf24_WriteReg(STATUS, 0x00);
-	}
-
-	else if (REG == FIFO_STATUS)
-	{
-		nrf24_WriteReg(FIFO_STATUS, 0x11);
-	}
-
-	else {
-	nrf24_WriteReg(CONFIG, 0x08);
-	nrf24_WriteReg(EN_AA, 0x3F);
-	nrf24_WriteReg(EN_RXADDR, 0x03);
-	nrf24_WriteReg(SETUP_AW, 0x03);
-	nrf24_WriteReg(SETUP_RETR, 0x03);
-	nrf24_WriteReg(RF_CH, 0x02);
-	nrf24_WriteReg(RF_SETUP, 0x0E);
-	nrf24_WriteReg(STATUS, 0x00);
-	nrf24_WriteReg(OBSERVE_TX, 0x00);
-	nrf24_WriteReg(CD, 0x00);
-	uint8_t rx_addr_p0_def[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
-	nrf24_WriteRegMulti(RX_ADDR_P0, rx_addr_p0_def, 5);
-	uint8_t rx_addr_p1_def[5] = {0xC2, 0xC2, 0xC2, 0xC2, 0xC2};
-	nrf24_WriteRegMulti(RX_ADDR_P1, rx_addr_p1_def, 5);
-	nrf24_WriteReg(RX_ADDR_P2, 0xC3);
-	nrf24_WriteReg(RX_ADDR_P3, 0xC4);
-	nrf24_WriteReg(RX_ADDR_P4, 0xC5);
-	nrf24_WriteReg(RX_ADDR_P5, 0xC6);
-	uint8_t tx_addr_def[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
-	nrf24_WriteRegMulti(TX_ADDR, tx_addr_def, 5);
-	nrf24_WriteReg(RX_PW_P0, 0);
-	nrf24_WriteReg(RX_PW_P1, 0);
-	nrf24_WriteReg(RX_PW_P2, 0);
-	nrf24_WriteReg(RX_PW_P3, 0);
-	nrf24_WriteReg(RX_PW_P4, 0);
-	nrf24_WriteReg(RX_PW_P5, 0);
-	nrf24_WriteReg(FIFO_STATUS, 0x11);
-	nrf24_WriteReg(DYNPD, 0);
-	nrf24_WriteReg(FEATURE, 0);
-	}
+    if (REG == STATUS)
+    {
+        nrf24_WriteReg(STATUS, 0x00U);
+    }
+    else if (REG == FIFO_STATUS)
+    {
+        nrf24_WriteReg(FIFO_STATUS, 0x11U);
+    }
+    else
+    {
+        nrf24_WriteReg(CONFIG, 0x08U);
+        nrf24_WriteReg(EN_AA, 0x3FU);
+        nrf24_WriteReg(EN_RXADDR, 0x03U);
+        nrf24_WriteReg(SETUP_AW, 0x03U);
+        nrf24_WriteReg(SETUP_RETR, 0x03U);
+        nrf24_WriteReg(RF_CH, 0x02U);
+        nrf24_WriteReg(RF_SETUP, 0x0EU);
+        nrf24_WriteReg(STATUS, 0x00U);
+        nrf24_WriteReg(OBSERVE_TX, 0x00U);
+        nrf24_WriteReg(CD, 0x00U);
+        uint8_t rx_addr_p0_def[5] = {0xE7U, 0xE7U, 0xE7U, 0xE7U, 0xE7U};
+        nrf24_WriteRegMulti(RX_ADDR_P0, rx_addr_p0_def, 5);
+        uint8_t rx_addr_p1_def[5] = {0xC2U, 0xC2U, 0xC2U, 0xC2U, 0xC2U};
+        nrf24_WriteRegMulti(RX_ADDR_P1, rx_addr_p1_def, 5);
+        nrf24_WriteReg(RX_ADDR_P2, 0xC3U);
+        nrf24_WriteReg(RX_ADDR_P3, 0xC4U);
+        nrf24_WriteReg(RX_ADDR_P4, 0xC5U);
+        nrf24_WriteReg(RX_ADDR_P5, 0xC6U);
+        uint8_t tx_addr_def[5] = {0xE7U, 0xE7U, 0xE7U, 0xE7U, 0xE7U};
+        nrf24_WriteRegMulti(TX_ADDR, tx_addr_def, 5);
+        nrf24_WriteReg(RX_PW_P0, 0U);
+        nrf24_WriteReg(RX_PW_P1, 0U);
+        nrf24_WriteReg(RX_PW_P2, 0U);
+        nrf24_WriteReg(RX_PW_P3, 0U);
+        nrf24_WriteReg(RX_PW_P4, 0U);
+        nrf24_WriteReg(RX_PW_P5, 0U);
+        nrf24_WriteReg(FIFO_STATUS, 0x11U);
+        nrf24_WriteReg(DYNPD, 0U);
+        nrf24_WriteReg(FEATURE, 0U);
+    }
 }
 
-
-
-
-void NRF24_Init (void)
+void NRF24_Init(void)
 {
-	// disable the chip before configuring the device
-	CE_Disable();
-
-
-	// reset everything
-	nrf24_reset (0);
-
-	nrf24_WriteReg(CONFIG, 0);  // will be configured later
-
-	nrf24_WriteReg(EN_AA, 0);  // No Auto ACK
-
-	nrf24_WriteReg (EN_RXADDR, 0);  // Not Enabling any data pipe right now
-
-	nrf24_WriteReg (SETUP_AW, 0x03);  // 5 Bytes for the TX/RX address
-
-	nrf24_WriteReg (SETUP_RETR, 0);   // No retransmission
-
-	nrf24_WriteReg (RF_CH, 0);  // will be setup during Tx or RX
-
-	nrf24_WriteReg (RF_SETUP, 0x0E);   // Power= 0db, data rate = 2Mbps
-
-	// Enable the chip after configuring the device
-	CE_Enable();
-
+    CE_Disable();
+    nrf24_reset(0U);
+    nrf24_WriteReg(CONFIG, 0U);
+    nrf24_WriteReg(EN_AA, 0U);
+    nrf24_WriteReg(EN_RXADDR, 0U);
+    nrf24_WriteReg(SETUP_AW, 0x03U);
+    nrf24_WriteReg(SETUP_RETR, 0U);
+    nrf24_WriteReg(RF_CH, 0U);
+    nrf24_WriteReg(RF_SETUP, 0x0EU);
+    CE_Enable();
 }
 
-
-// set up the Tx mode
-
-void NRF24_TxMode (uint8_t *Address, uint8_t channel)
+void NRF24_TxMode(uint8_t *Address, uint8_t channel)
 {
-	// disable the chip before configuring the device
-	CE_Disable();
+    CE_Disable();
+    nrf24_WriteReg(RF_CH, channel);
+    nrf24_WriteRegMulti(TX_ADDR, Address, 5);
 
-	nrf24_WriteReg (RF_CH, channel);  // select the channel
-
-	nrf24_WriteRegMulti(TX_ADDR, Address, 5);  // Write the TX address
-
-
-	// power up the device
-	uint8_t config = nrf24_ReadReg(CONFIG);
-//	config = config | (1<<1);   // write 1 in the PWR_UP bit
-	config = config & (0xF2);    // write 0 in the PRIM_RX, and 1 in the PWR_UP, and all other bits are masked
-	nrf24_WriteReg (CONFIG, config);
-
-	// Enable the chip after configuring the device
-	CE_Enable();
+    uint8_t config = nrf24_ReadReg(CONFIG);
+    config = (uint8_t)((config & 0xF2U));
+    nrf24_WriteReg(CONFIG, config);
+    CE_Enable();
 }
 
-
-// transmit the data
-
-uint8_t NRF24_Transmit (uint8_t *data)
+uint8_t NRF24_Transmit(uint8_t *data)
 {
-	uint8_t cmdtosend = 0;
+    uint8_t cmdtosend;
 
-	// select the device
-	CS_Select();
+    CS_Select();
+    cmdtosend = W_TX_PAYLOAD;
+    (void)HAL_SPI_Transmit(NRF24_SPI, &cmdtosend, 1U, 100U);
+    (void)HAL_SPI_Transmit(NRF24_SPI, data, 32U, 1000U);
+    CS_UnSelect();
 
-	// payload command
-	cmdtosend = W_TX_PAYLOAD;
-	HAL_SPI_Transmit(NRF24_SPI, &cmdtosend, 1, 100);
+    HAL_Delay(1U); // MISRA C 2012 Rule 21.6
 
-	// send the payload
-	HAL_SPI_Transmit(NRF24_SPI, data, 32, 1000);
+    uint8_t fifostatus = nrf24_ReadReg(FIFO_STATUS);
 
-	// Unselect the device
-	CS_UnSelect();
+    if (((fifostatus & (1U << 4)) != 0U) && ((fifostatus & (1U << 3)) == 0U))
+    {
+        cmdtosend = FLUSH_TX;
+        nrfsendCmd(cmdtosend);
+        nrf24_reset(FIFO_STATUS);
+        return 1U;
+    }
 
-	HAL_Delay(1);
-
-	uint8_t fifostatus = nrf24_ReadReg(FIFO_STATUS);
-
-	// check the fourth bit of FIFO_STATUS to know if the TX fifo is empty
-	if ((fifostatus&(1<<4)) && (!(fifostatus&(1<<3))))
-	{
-		cmdtosend = FLUSH_TX;
-		nrfsendCmd(cmdtosend);
-
-		// reset FIFO_STATUS
-		nrf24_reset (FIFO_STATUS);
-
-		return 1;
-	}
-
-	return 0;
+    return 0U;
 }
 
-
-void NRF24_RxMode (uint8_t *Address, uint8_t channel)
+void NRF24_RxMode(uint8_t *Address, uint8_t channel)
 {
-	// disable the chip before configuring the device
-	CE_Disable();
+    CE_Disable();
+    nrf24_reset(STATUS);
+    nrf24_WriteReg(RF_CH, channel);
 
-	nrf24_reset (STATUS);
+    uint8_t en_rxaddr = nrf24_ReadReg(EN_RXADDR);
+    en_rxaddr = (uint8_t)(en_rxaddr | (1U << 2));
+    nrf24_WriteReg(EN_RXADDR, en_rxaddr);
 
-	nrf24_WriteReg (RF_CH, channel);  // select the channel
+    nrf24_WriteRegMulti(RX_ADDR_P1, Address, 5);
+    nrf24_WriteReg(RX_ADDR_P2, 0xEEU);
+    nrf24_WriteReg(RX_PW_P2, 32U);
 
-	// select data pipe 2
-	uint8_t en_rxaddr = nrf24_ReadReg(EN_RXADDR);
-	en_rxaddr = en_rxaddr | (1<<2);
-	nrf24_WriteReg (EN_RXADDR, en_rxaddr);
+    uint8_t config = nrf24_ReadReg(CONFIG);
+    config = (uint8_t)(config | 0x03U);
+    nrf24_WriteReg(CONFIG, config);
 
-	/* We must write the address for Data Pipe 1, if we want to use any pipe from 2 to 5
-	 * The Address from DATA Pipe 2 to Data Pipe 5 differs only in the LSB
-	 * Their 4 MSB Bytes will still be same as Data Pipe 1
-	 *
-	 * For Eg->
-	 * Pipe 1 ADDR = 0xAABBCCDD11
-	 * Pipe 2 ADDR = 0xAABBCCDD22
-	 * Pipe 3 ADDR = 0xAABBCCDD33
-	 *
-	 */
-	nrf24_WriteRegMulti(RX_ADDR_P1, Address, 5);  // Write the Pipe1 address
-	nrf24_WriteReg(RX_ADDR_P2, 0xEE);  // Write the Pipe2 LSB address
-
-	nrf24_WriteReg (RX_PW_P2, 32);   // 32 bit payload size for pipe 2
-
-
-	// power up the device in Rx mode
-	uint8_t config = nrf24_ReadReg(CONFIG);
-	config = config | (1<<1) | (1<<0);
-	nrf24_WriteReg (CONFIG, config);
-
-	// Enable the chip after configuring the device
-	CE_Enable();
+    CE_Enable();
 }
 
-
-uint8_t isDataAvailable (int pipenum)
+uint8_t isDataAvailable(int pipenum)
 {
-	uint8_t status = nrf24_ReadReg(STATUS);
-
-	if ((status&(1<<6))&&(status&(pipenum<<1)))
-	{
-
-		nrf24_WriteReg(STATUS, (1<<6));
-
-		return 1;
-	}
-
-	return 0;
+    uint8_t status = nrf24_ReadReg(STATUS);
+    if (((status & (1U << 6)) != 0U) && ((status & (uint8_t)(pipenum << 1)) != 0U))
+    {
+        nrf24_WriteReg(STATUS, (1U << 6));
+        return 1U;
+    }
+    return 0U;
 }
 
-
-void NRF24_Receive (uint8_t *data)
+void NRF24_Receive(uint8_t *data)
 {
-	uint8_t cmdtosend = 0;
+    uint8_t cmdtosend;
 
-	// select the device
-	CS_Select();
+    CS_Select();
+    cmdtosend = R_RX_PAYLOAD;
+    (void)HAL_SPI_Transmit(NRF24_SPI, &cmdtosend, 1U, 100U);
+    (void)HAL_SPI_Receive(NRF24_SPI, data, 32U, 1000U);
+    CS_UnSelect();
 
-	// payload command
-	cmdtosend = R_RX_PAYLOAD;
-	HAL_SPI_Transmit(NRF24_SPI, &cmdtosend, 1, 100);
+    HAL_Delay(1U);
 
-	// Receive the payload
-	HAL_SPI_Receive(NRF24_SPI, data, 32, 1000);
-
-	// Unselect the device
-	CS_UnSelect();
-
-	HAL_Delay(1);
-
-	cmdtosend = FLUSH_RX;
-	nrfsendCmd(cmdtosend);
+    cmdtosend = FLUSH_RX;
+    nrfsendCmd(cmdtosend);
 }
 
-
-
-// Read all the Register data
-void NRF24_ReadAll (uint8_t *data)
+void NRF24_ReadAll(uint8_t *data)
 {
-	for (int i=0; i<10; i++)
-	{
-		*(data+i) = nrf24_ReadReg(i);
-	}
+    for (int i = 0; i < 10; ++i)
+    {
+        data[i] = nrf24_ReadReg((uint8_t)i);
+    }
 
-	nrf24_ReadReg_Multi(RX_ADDR_P0, (data+10), 5);
+    nrf24_ReadReg_Multi(RX_ADDR_P0, &data[10], 5);
+    nrf24_ReadReg_Multi(RX_ADDR_P1, &data[15], 5);
 
-	nrf24_ReadReg_Multi(RX_ADDR_P1, (data+15), 5);
+    data[20] = nrf24_ReadReg(RX_ADDR_P2);
+    data[21] = nrf24_ReadReg(RX_ADDR_P3);
+    data[22] = nrf24_ReadReg(RX_ADDR_P4);
+    data[23] = nrf24_ReadReg(RX_ADDR_P5);
 
-	*(data+20) = nrf24_ReadReg(RX_ADDR_P2);
-	*(data+21) = nrf24_ReadReg(RX_ADDR_P3);
-	*(data+22) = nrf24_ReadReg(RX_ADDR_P4);
-	*(data+23) = nrf24_ReadReg(RX_ADDR_P5);
+    nrf24_ReadReg_Multi(RX_ADDR_P0, &data[24], 5);
 
-	nrf24_ReadReg_Multi(RX_ADDR_P0, (data+24), 5);
-
-	for (int i=29; i<38; i++)
-	{
-		*(data+i) = nrf24_ReadReg(i-12);
-	}
-
+    for (int i = 29; i < 38; ++i)
+    {
+        data[i] = nrf24_ReadReg((uint8_t)(i - 12));
+    }
 }
-
-
-
-
-
-
-
-
-
